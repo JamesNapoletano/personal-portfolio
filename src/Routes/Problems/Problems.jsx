a6b5a305c35e729c3b8ebe8fe5332c4cc195937a
james <jnap9311@gmail.com>
2022-08-20 15:55:11 -0400

    probNav style

--- src/Routes/Problems/Problems.jsx
+++ src/Routes/Problems/Problems.jsx
@@ -1,4 +1,5 @@
 import { Link, Outlet, useLocation } from "react-router-dom";
+import styles from './Problems.module.css'
 
 
 
@@ -9,6 +10,7 @@ export default function Problems() {
     const fizzBuzz = (
         <Link
             to={pathname !== '/personal-portfolio/problems/fizzbuzz' ? 'fizzbuzz' : ''}
+            className={pathname === '/personal-portfolio/problems/fizzbuzz' ? styles.probNavBtnSelected : styles.probNavBtn}
         >
             FizzBuzz
         </Link>
@@ -16,6 +18,7 @@ export default function Problems() {
     const wordPalindrome = (
         <Link
             to={pathname !== '/personal-portfolio/problems/wordpalindrome' ? 'wordpalindrome' : ''}
+            className={pathname === '/personal-portfolio/problems/wordpalindrome' ? styles.probNavBtnSelected : styles.probNavBtn}
         >
             Word Palindrome
         </Link>
@@ -23,6 +26,7 @@ export default function Problems() {
     const numberPalindrome = (
         <Link
             to={pathname !== '/personal-portfolio/problems/numberpalindrome' ? 'numberpalindrome' : ''}
+            className={pathname === '/personal-portfolio/problems/numberpalindrome' ? styles.probNavBtnSelected : styles.probNavBtn}
         >
             Number Palindrome
         </Link>
@@ -30,6 +34,7 @@ export default function Problems() {
     const twoSum = (
         <Link
             to={pathname !== '/personal-portfolio/problems/twosum' ? 'twosum' : ''}
+            className={pathname === '/personal-portfolio/problems/twosum' ? styles.probNavBtnSelected : styles.probNavBtn}
         >
             Two Sum
         </Link>
@@ -37,7 +42,7 @@ export default function Problems() {
 
     return (
         <div>
-            <nav>
+            <nav className={styles.probNav}>
                 {fizzBuzz}
                 {wordPalindrome}
                 {numberPalindrome}

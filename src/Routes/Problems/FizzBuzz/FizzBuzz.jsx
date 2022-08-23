b37280340f0f60abbcaaff400a3bb19b3448c5af
james <jnap9311@gmail.com>
2022-08-22 22:55:33 -0400

    fixed FizzBuzz functionality

--- src/Routes/Problems/FizzBuzz/FizzBuzz.jsx
+++ src/Routes/Problems/FizzBuzz/FizzBuzz.jsx
@@ -1,31 +1,41 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const FizzBuzz = () => {
     const [inputValue, setInputValue] = useState('')
     const [outputValue, setOutputValue] = useState('')
+    const [submittedValue, setSubmittedValue] = useState('')
+
+    useEffect(() => {
+        if (inputValue === '') {
+            setOutputValue('')
+        }
+    }, [inputValue])
 
     const submit = () => {
-        if(inputValue < 1000 && inputValue >= 1) {
+        if (inputValue < 1000 && inputValue > 0) {
+            setSubmittedValue(inputValue)
             fizzBuzzGen(inputValue)
+        } else if (inputValue === '') {
+            setOutputValue('')
         } else {
             return setOutputValue('Please enter a number between 1 and 999')
         }
     }
 
     const fizzBuzzGen = (num) => {
-        const fBOutput = []
+        const genOutput = []
         for (var i = 1; i <= num; i++) {
             if (i % 3 === 0) {
-                fBOutput.push('Fizz' + (i % 5 === 0 ? 'Buzz' : ''))
+                genOutput.push('Fizz' + (i % 5 === 0 ? 'Buzz' : ''))
             }
             else if (i % 5 === 0) {
-                fBOutput.push('Buzz')
+                genOutput.push('Buzz')
             }
             else {
-                fBOutput.push(i.toString())
+                genOutput.push(i.toString())
             }
         }
-        setOutputValue(fBOutput.join(', '))
+        setOutputValue(genOutput.join(', '))
     }
 
     return (
@@ -33,10 +43,35 @@ const FizzBuzz = () => {
             <h1>
                 FizzBuzz
             </h1>
-            <input type='number' value={inputValue} onChange={(e) => {setInputValue(e.target.value)}} />
+            <input type='number' value={inputValue} onChange={(e) => { setInputValue(e.target.value) }} />
             <button onClick={submit}>Submit</button>
             <br />
-            <p>{outputValue}</p>
+            <div>
+                {outputValue ? <>
+                    <h2>
+                        Limit: {submittedValue}
+                    </h2>
+                    <div>
+                        {outputValue}
+                    </div>
+                </> :
+                    <>
+                        <h2>Welcome to FizzBuzz!</h2>
+                        <div>
+                            The concept is simple: enter an upper limit for the FizzBuzz generator.
+                            <br />
+                            Returned, will be, all numbers from 1 to your specified limit. All numbers
+                            <br />
+                            divisible by 3 will be replaced with "Fizz," while all numbers divisible by
+                            <br />
+                            5 will be replaced with "Buzz." All numbers divisible by both 3 and 5 will
+                            <br />
+                            be replaced with "FizzBuzz."
+                            <br />
+                            Enjoy!
+                        </div>
+                    </>
+                }</div>
         </div>
     )
 }

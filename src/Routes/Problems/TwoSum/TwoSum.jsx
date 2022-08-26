0fc9065ebd2f0e7b3b73e411bab1d1f880672089
james <jnap9311@gmail.com>
2022-08-26 18:11:54 -0400

    Two Sum page functionality for inputs, buttons, contentBox

--- src/Routes/Problems/TwoSum/TwoSum.jsx
+++ src/Routes/Problems/TwoSum/TwoSum.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import styles from './TwoSum.module.css'
 
 const TwoSum = () => {
@@ -7,7 +7,60 @@ const TwoSum = () => {
     const [inputValueThree, setInputValueThree] = useState('')
     const [inputValueFour, setInputValueFour] = useState('')
     const [targetValue, setTargetValue] = useState('')
+    const [submittedCheckValues, setSubmittedCheckValues] = useState('')
+    const [submittedTargetValue, setSubmittedTargetValue] = useState('')
+    const [outputValue, setOutputValue] = useState('')
 
+    useEffect(() => {
+        if (inputValueOne === '' &&
+            inputValueTwo === '' &&
+            inputValueThree === '' &&
+            inputValueFour === '' &&
+            targetValue === '') {
+            setOutputValue('')
+        }
+    }, [inputValueOne, inputValueTwo, inputValueThree, inputValueFour, targetValue, outputValue])
+
+    const submit = () => {
+        const subValArr = []
+        if (inputValueOne === '' &&
+            inputValueTwo === '' &&
+            inputValueThree === '' &&
+            inputValueFour === '' &&
+            targetValue === '') {
+            setOutputValue('')
+        } else {
+            if (Number(inputValueOne) !== 0 && inputValueOne !== '') {
+                subValArr.push(inputValueOne)
+            }
+            if (Number(inputValueTwo) !== 0 && inputValueTwo !== '') {
+                subValArr.push(inputValueTwo)
+            }
+            if (Number(inputValueThree) !== 0 && inputValueThree !== '') {
+                subValArr.push(inputValueThree)
+            }
+            if (Number(inputValueFour) !== 0 && inputValueFour !== '') {
+                subValArr.push(inputValueFour)
+            }
+            if (Number(targetValue)) {
+                setSubmittedTargetValue(targetValue)
+            }
+            setSubmittedCheckValues(subValArr.join(', '))
+            if (subValArr.length < 2 || targetValue === '') {
+                setOutputValue('')
+            } else {
+                setOutputValue(`${subValArr.join(', ')}, ${targetValue}`)
+            }
+        }
+    }
+
+    const clear = () => {
+        setInputValueOne('')
+        setInputValueTwo('')
+        setInputValueThree('')
+        setInputValueFour('')
+        setTargetValue('')
+    }
 
     return (
         <div>
@@ -22,13 +75,42 @@ const TwoSum = () => {
                     <input type='number' value={inputValueThree} onChange={(e) => { setInputValueThree(e.target.value) }} />
                     <input type='number' value={inputValueFour} onChange={(e) => { setInputValueFour(e.target.value) }} />
                 </div>
-                <button>
-                    Submit
-                </button>
+                <div className={styles.twoSumButtons}>
+                    <button onClick={submit}>
+                        Submit
+                    </button>
+                    <button onClick={clear}>
+                        Clear
+                    </button>
+                </div>
                 <div className={styles.twoSumInputs}>
                     <p>Target Value Input:</p>
                     <input type='number' value={targetValue} onChange={(e) => { setTargetValue(e.target.value) }} />
                 </div>
+            </div>
+            <div className={styles.contentBox}>
+                {outputValue ? <>
+                    <h2>
+                        Check Values: {submittedCheckValues}
+                        <br />
+                        Target Value: {submittedTargetValue}
+                    </h2>
+                    <div>
+                        {outputValue}
+                    </div>
+                </> :
+                    <>
+                        <h2>Welcome to Two Sum!</h2>
+                        <div>
+                            Two Sum is a calculator of sorts. You can input between 2-4 numbers
+                            into "Check Value Inputs" and a single number into "Target Value Input."
+                            You can then click Submit and see that the calculator will return which
+                            two check values equal the target value.
+                            <br />
+                            Enjoy!
+                        </div>
+                    </>
+                }
             </div>
         </div>
     )

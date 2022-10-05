ca3174301cd65bfc402fb39a0e9ab88acb3b7cc4
james <jnap9311@gmail.com>
2022-10-04 22:37:10 -0400

    Populated Contact page with data.

--- src/Routes/Contact/Contact.jsx
+++ src/Routes/Contact/Contact.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react'
 import { useContext } from 'react'
-import UnderDevelopment from '../../Components/UnderDevelopment/UnderDevelopment'
 import { AccentColorContext } from '../../context/accent-color-context'
+import styles from './Contact.module.css'
 
 export default function Contact() {
     const { updateAccentColor } = useContext(AccentColorContext)
@@ -9,11 +9,60 @@ export default function Contact() {
     useEffect(() => {
         updateAccentColor('steel')
     }, [])
-    
+
     return (
         <>
             <h1>Contact</h1>
-            <UnderDevelopment />
+            <div className={styles.contentCard}>
+                <ul>
+                    <li>
+                        <p>
+                            Name
+                        </p>
+                        <p>
+                            James Napoletano
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            E-Mail
+                        </p>
+                        <p>
+                            <a href='mailto:jnap9311@gmail.com'>
+                                jnap9311@gmail.com
+                            </a>
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            Phone
+                        </p>
+                        <p>
+                            1 (586) 719-4219
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            GitHub
+                        </p>
+                        <p>
+                            <a href='https://github.com/JamesNapoletano'>
+                                https://github.com/JamesNapoletano
+                            </a>
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            Resume
+                        </p>
+                        <p>
+                            <a href='mailto:jnap9311@gmail.com?subject=Resume%20Inquiry&body=Tell%20me%20about%20yourself.'>
+                                Inquire
+                            </a>
+                        </p>
+                    </li>
+                </ul>
+            </div>
         </>
     )
 }
